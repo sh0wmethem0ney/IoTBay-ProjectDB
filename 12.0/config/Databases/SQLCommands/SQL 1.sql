6e192f43430d3555697e0a69c8e254722d14a1f6
Kwon <lemon3840@gmail.com>
2025-04-30 16:08:59 +1000

    second

---
+++ 12.0/config/Databases/SQLCommands/SQL 1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Customers (
+    customer_id VARCHAR(8) PRIMARY KEY,
+    customer_name VARCHAR(50),
+    date_of_birth DATE,
+    phone_number VARCHAR(12),
+    address VARCHAR(100),
+    password VARCHAR(12),
+    email VARCHAR(50),
+    gender VARCHAR(10)
+);
